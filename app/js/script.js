5f8b72e1059e03c5109869c3a3c772c105dd402a
Mostafa Saeed <https://github.com/Desha-Saeed>
2022-04-28 20:57:17 +0200

    added warning message if no rating

--- app/js/script.js
+++ app/js/script.js
@@ -1,45 +1,34 @@
-
 //Grabbing the elements
-const ratingEl = document.querySelectorAll('.card__rating');
-const submitEl = document.querySelector('.card__submit');
-const thanksCard = document.querySelector('.thanks-card');
-const ratingCard = document.querySelector('.rating-card');
-const ratings = document.querySelectorAll('.card__rating');
-const result = document.getElementById('result');
-
-
-
-
-
-
-
+const ratingEl = document.querySelectorAll(".card__rating");
+const submitEl = document.querySelector(".card__submit");
+const thanksCard = document.querySelector(".thanks-card");
+const ratingCard = document.querySelector(".rating-card");
+const ratings = document.querySelectorAll(".card__rating");
+const result = document.getElementById("result");
 
 //rating implementation
-ratingEl.forEach(element => {
-    //adding active if clicked
-    element.addEventListener('click', ()=>{
-        ratingEl.forEach(element => element.classList.remove('active'));
-        element.classList.add('active');
-
-    })
+ratingEl.forEach((element) => {
+  //adding active if clicked
+  element.addEventListener("click", () => {
+    ratingEl.forEach((element) => element.classList.remove("active"));
+    element.classList.add("active");
+  });
 });
 
+//submit button 
 
-//submit button flip
+submitEl.addEventListener("click", () => {
+  ratingCard.style.display = "none";
 
-submitEl.addEventListener('click', ()=>{
+  thanksCard.style.display = "block";
 
-    ratingCard.style.display ='none';
-
-    thanksCard.style.display = 'block';
-
-    ratings.forEach(rating => {
-        if(rating.classList.contains('active')){
-            result.innerHTML = `You Selected ${rating.innerHTML} out of 5`
-    
-        }
-        
-    });
+  result.innerText = "Please give us a rating";
 
 
-})
+  //add the rating result
+  ratings.forEach((rating) => {
+    if (rating.classList.contains("active")) {
+      result.innerText = `You selected ${rating.innerHTML} out of 5`;
+    }
+  });
+});
